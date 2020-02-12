570a35822df8889a440c8fddcd0876cf716bafa9
SabinaAbbasova <sabina.abbasova2909@gmail.com>
2020-02-12 17:14:52 +0400

    Update loading.js

--- loading.js
+++ loading.js
@@ -6,42 +6,26 @@ const cta = document.querySelector('.cta');
 const logo = document.querySelector('.logo');
 const scrolling = document.querySelector('.scrolling');
 const mainSections = document.querySelector('main');
-// const footer = document.querySelector('footer');
-
-
 function loading() {
     setTimeout(() => {
         wrapper.style.opacity = 0;
         wrapper.style.display = 'none';
-
         navbar.style.display = 'block';
         setTimeout(() => {navbar.style.opacity = 1}, 50);
-
         header.style.display = 'block';
         setTimeout(() => {header.style.opacity = 1}, 50);
-
         headerText.style.display = 'block';
         setTimeout(() => {headerText.style.opacity = 1}, 50);
-
         cta.style.display = 'block';
         setTimeout(() => {cta.style.opacity = 1}, 50);
-
         logo.style.display = 'block';
         setTimeout(() => {logo.style.opacity = 1}, 50);
-
         scrolling.style.display = 'block';
         setTimeout(() => {scrolling.style.opacity = 1}, 50);
-
         mainSections.style.display = 'block';
         setTimeout(() => {mainSections.style.opacity = 1}, 50);
-
-        // footer.style.display = 'block';
-        // setTimeout(() => (footer.style.opacity = 1), 50);
-
-
     }, 250);
 }
-
 document.addEventListener('DOMContentLoaded', function() {
    window.addEventListener("load",  function() {
            setTimeout(() => {loading()}, 500);
